210f5bbd16005cef64952f6a3009e4e3c06cb893
Teja Karlapudi <teja2495@gmail.com>
2025-07-31 19:09:05 -0400

    feat: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.tk.quickcontacts"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
